0e8edd994af30f68854e1c804d7d1bdca9b5d88a
Florian LABADIE <florian.labadie@epitech.eu>
2024-05-14 17:14:49 +0200

    feat : actions instruction and fix : cond_instrcution

--- src/instruct/actions_instruction.c
+++ src/instruct/actions_instruction.c
@@ -7,17 +7,81 @@
 
 #include "my.h"
 
+static void sti_loop(corewar_t *corewar, int *args,
+    instruct_types_t *types, int prog_nbr)
+{
+    uint32_t load = 0;
+
+    for (int i = 1; i < 3; i += 1) {
+        if (types[i] == REGISTER)
+            load += corewar->champions[prog_nbr]->regs[args[i]];
+        if (types[i] == DIRECT || types[i] == INDIRECT)
+            load += args[i];
+    }
+    corewar->arena[corewar->champions[prog_nbr]->current_PC +
+            load % IDX_MOD] = corewar->champions[prog_nbr]->regs[args[0]];
+}
+
 int st_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
 {
+    int *args = NULL;
+    instruct_types_t *types =
+        get_instruct_types(corewar->arena[champion[prog_nbr]->PC + 1]);
+
+    champion[prog_nbr]->current_PC = champion[prog_nbr]->PC;
+    if (!types)
+        return KO;
+    champion[prog_nbr]->nbr_cycle += op_tab[ST].nbr_cycles;
+    champion[prog_nbr]->PC += 2;
+    args = parse_parameter(corewar, types, ST, champion);
+    if (!args)
+        return KO;
+    if (types[1] == REGISTER)
+        corewar->champions[prog_nbr]->regs[args[1]] =
+            corewar->champions[prog_nbr]->regs[args[0]];
+    if (types[1] == INDIRECT)
+        corewar->arena[corewar->champions[prog_nbr]->current_PC +
+            args[1] % IDX_MOD] = corewar->champions[prog_nbr]->regs[args[0]];
+    free(args);
     return OK;
 }
 
 int sti(corewar_t *corewar, champion_t **champion, int prog_nbr)
 {
+    int *args = NULL;
+    instruct_types_t *types =
+        get_instruct_types(corewar->arena[champion[prog_nbr]->PC + 1]);
+
+    champion[prog_nbr]->current_PC = champion[prog_nbr]->PC;
+    if (!types)
+        return KO;
+    champion[prog_nbr]->nbr_cycle += op_tab[STI].nbr_cycles;
+    champion[prog_nbr]->PC += 2;
+    args = parse_parameter(corewar, types, STI, champion);
+    if (!args)
+        return KO;
+    sti_loop(corewar, args, types, prog_nbr);
+    free(args);
     return OK;
 }
 
 int aff(corewar_t *corewar, champion_t **champion, int prog_nbr)
 {
+    int *args = NULL;
+    instruct_types_t *types =
+        get_instruct_types(corewar->arena[champion[prog_nbr]->PC + 1]);
+    uint16_t aff = 0;
+
+    champion[prog_nbr]->current_PC = champion[prog_nbr]->PC;
+    if (!types)
+        return KO;
+    champion[prog_nbr]->nbr_cycle += op_tab[AFF].nbr_cycles;
+    champion[prog_nbr]->PC += 2;
+    args = parse_parameter(corewar, types, AFF, champion);
+    if (!args)
+        return KO;
+    aff = corewar->champions[prog_nbr]->regs[args[0]] % (IDX_MOD / 2);
+    my_putchar(aff);
+    free(args);
     return OK;
 }
--- src/instruct/cond_instruction.c
+++ src/instruct/cond_instruction.c
@@ -7,6 +7,24 @@
 
 #include "my.h"
 
+static uint32_t *get_value_loop(corewar_t *corewar, int *args,
+    instruct_types_t *types, int prog_nbr)
+{
+    uint32_t *load = {0};
+
+    for (int i = 0; i < 2; i += 1) {
+        if (types[i] == REGISTER)
+            load[i] = corewar->champions[prog_nbr]->regs[args[i]];
+        if (types[i] == DIRECT)
+            load[i] = args[i];
+        if (types[i] == INDIRECT)
+            load[i] = *((uint32_t *)my_uint8_ndup
+            (&corewar->arena[corewar->champions[prog_nbr]->current_PC +
+            args[i] % IDX_MOD], REG_SIZE));
+    }
+    return load;
+}
+
 int add(corewar_t *corewar, champion_t **champion, int prog_nbr)
 {
     int *args = NULL;
@@ -22,7 +40,8 @@ int add(corewar_t *corewar, champion_t **champion, int prog_nbr)
     args = parse_parameter(corewar, types, ADD, champion);
     if (!args)
         return KO;
-    champion[prog_nbr]->regs[args[2]] = args[0] + args[1];
+    champion[prog_nbr]->regs[args[2]] = champion[prog_nbr]->regs[args[0]] +
+        champion[prog_nbr]->regs[args[1]];
     free(args);
     return OK;
 }
@@ -40,17 +59,19 @@ int sub(corewar_t *corewar, champion_t **champion, int prog_nbr)
     args = parse_parameter(corewar, types, SUB, champion);
     if (!args)
         return KO;
-    champion[prog_nbr]->regs[args[2]] = args[0] - args[1] < 0 ?
-        -(args[0] - args[1]) : args[0] - args[1];
+    champion[prog_nbr]->regs[args[2]] = champion[prog_nbr]->regs[args[0]] -
+        champion[prog_nbr]->regs[args[1]];
     free(args);
     return OK;
 }
+
 
 int and_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
 {
     int *args = NULL;
     instruct_types_t *types =
         get_instruct_types(corewar->arena[champion[prog_nbr]->PC + 1]);
+    uint32_t *load = {0};
 
     if (!types)
         return KO;
@@ -61,7 +82,9 @@ int and_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
     args = parse_parameter(corewar, types, AND, champion);
     if (!args)
         return KO;
-    champion[prog_nbr]->regs[args[2]] = args[0] & args[1];
+    load = get_value_loop(corewar, args, types, prog_nbr);
+    corewar->champions[prog_nbr]->regs[args[1]] = load[0] & load[1];
+    free(load);
     free(args);
     return OK;
 }
@@ -71,6 +94,7 @@ int or_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
     int *args = NULL;
     instruct_types_t *types =
         get_instruct_types(corewar->arena[champion[prog_nbr]->PC + 1]);
+    uint32_t *load = {0};
 
     if (!types)
         return KO;
@@ -79,7 +103,9 @@ int or_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
     args = parse_parameter(corewar, types, OR, champion);
     if (!args)
         return KO;
-    champion[prog_nbr]->regs[args[2]] = args[0] | args[1];
+    load = get_value_loop(corewar, args, types, prog_nbr);
+    corewar->champions[prog_nbr]->regs[args[1]] = load[0] | load[1];
+    free(load);
     free(args);
     return OK;
 }
@@ -89,6 +115,7 @@ int xor_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
     int *args = NULL;
     instruct_types_t *types =
         get_instruct_types(corewar->arena[champion[prog_nbr]->PC + 1]);
+    uint32_t *load = {0};
 
     if (!types)
         return KO;
@@ -97,7 +124,9 @@ int xor_i(corewar_t *corewar, champion_t **champion, int prog_nbr)
     args = parse_parameter(corewar, types, XOR, champion);
     if (!args)
         return KO;
-    champion[prog_nbr]->regs[args[2]] = args[0] ^ args[1];
+    load = get_value_loop(corewar, args, types, prog_nbr);
+    corewar->champions[prog_nbr]->regs[args[1]] = load[0] ^ load[1];
+    free(load);
     free(args);
     return OK;
 }
